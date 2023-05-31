4b34403e88c19df171ca39c57897f5886028fd63
Himanshu Gupta <thinktank.himanshu@gmail.com>
2023-05-31 13:07:55 +0530

    vercel analytics implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/tailwind.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
